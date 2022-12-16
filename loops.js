16be7a1cc7118924c7c88be539829b96c91c7897
srijal gupta <guptasrijal0901@gmail.com>
2022-12-16 20:22:39 +0530

    forloop

--- loops.js
+++ loops.js
@@ -1,35 +1,40 @@
-// for ( let index=0; index<100; index+1){
-//     console.log("my name is srijal gupta");
-// }
-// // console.log("loop completed");
-// // here this code will run for 100th times 
-// // zero se start hoga for jaise jaise aage bhadega vaise vaise index 
-// // value inc hogi or loop tb tk chalega jbtk lastcondition tk satisfy na ho jaye 
-// //  aab index value pata karte hai kitne baar loop chala */
+// // for ( let index=0; index<100; index+1){
+// //     console.log("my name is srijal gupta");
+// // }
+// // // console.log("loop completed");
+// // // here this code will run for 100th times 
+// // // zero se start hoga for jaise jaise aage bhadega vaise vaise index 
+// // // value inc hogi or loop tb tk chalega jbtk lastcondition tk satisfy na ho jaye 
+// // //  aab index value pata karte hai kitne baar loop chala */
 
-// for (let index = 0; index <=20 ; index = index + 1) {
-//       console.log("the current value of index is", index);
-//      }
+// // for (let index = 0; index <=20 ; index = index + 1) {
+// //       console.log("the current value of index is", index);
+// //      }
 
 
-    //  now lets learn about uses of Array
-    let a = ["Post 1", "Post 2", "Post 3", "Post 4", "Post 5", "Post 6", "Post 7"];
-    console.log(a.length);
+//     //  now lets learn about uses of Array
+//     let a = ["Post 1", "Post 2", "Post 3", "Post 4", "Post 5", "Post 6", "Post 7"];
+//     console.log(a.length);
 
-    for (let index = 0; index < a.length; index++) {
-        console.log("The element at ", 7, "position of array is ", a[index]);
-        }
+//     for (let index = 0; index < a.length; index++) {
+//         console.log("The element at ", 7, "position of array is ", a[index]);
+//         }
         
-    let studentname = "srijal gupta";
-        let cgpa= 7.7;
+//     let studentname = "srijal gupta";
+//         let cgpa= 7.7;
 
-        // 1st way 
+//         // 1st way 
 
-        console.log("hello my name is" , studentname);
-        console.log("my cgpa is" ,cgpa);
-        console.log( studentname, "cgpa is " , cgpa);
+//         console.log("hello my name is" , studentname);
+//         console.log("my cgpa is" ,cgpa);
+//         console.log( studentname, "cgpa is " , cgpa);
 
-       // 2nd method
+//        // 2nd method
 
-       console.log('my name is ${studentname} and my cgpa is ${cgpa}');
+//        console.log(`my name is ${studentname} and my cgpa is ${cgpa}`);
     
+       let x = 3 ;
+       for( i=1 ; i<51 ; i++ ){
+       console.log (x);
+       x=x*3;
+    }
