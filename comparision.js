612d36fd3df9169abc9eca9fcc25846d07b35543
srijal gupta <guptasrijal0901@gmail.com>
2022-12-19 20:43:24 +0530

    object

---
+++ comparision.js
@@ -0,0 +1,14 @@
+// console.log("this is equality", 6 == "6");
+// console.log("this is equality", 6 == "9");
+// console.log("this is greater than", 6 > 8);
+// console.log("this is less than", 6 < 8);
+// console.log("this is equality", 6 !== 6);
+
+// strictly equal to sign is === we use to check equality 
+// !== strict inequality 
+// <= less than equal to 
+// >= greater than equal to 
+
+console.log(9 === 9);
+console.log(9 === "9"); 
+// this is in double inverted coma so this means this is string  
---
+++ object.js
@@ -0,0 +1,16 @@
+const family_member = {
+mother :"archana gupta" ,
+father : "vinod kumar gupta" ,
+sister1 : "shreya gupta" ,
+sister2 : "deepti gupta" 
+}
+
+console.log(family_member);
+console.log(family_member["mother"]);
+console.log(family_member["father"]);
+
+
+console.log(family_member.sister1);
+console.log(family_member.sister2);
+
+
